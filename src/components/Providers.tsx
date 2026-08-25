560dbfdbe9a967e30bfb5e43eec86c064a22fd50
Pankaj <sadyalpankaj789@gmail.com>
2026-08-25 20:30:29 +0530

    changes for opening black theme by default

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./ThemeProvider";
 
 export function Providers({ children, session }: { children: React.ReactNode, session?: any }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true}>
       <SessionProvider session={session}>{children}</SessionProvider>
     </ThemeProvider>
   );
